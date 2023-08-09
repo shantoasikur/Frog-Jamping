3d8ef30c1b04e4f627dd2a2f4a0f0329ea75f60a
Md.Asikur Rahaman <141675776+shantoasikur@users.noreply.github.com>
2023-08-10 04:48:10 +0600

    Add files via upload

---
+++ frog jumping.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+
+using namespace std;
+int main(){
+    int t;
+    int sum=0;
+    int a,b,k;
+    while(t--){
+
+        cin>>a;
+        cin>>b;
+        cin>>k;
+        for(int i=1;i<=k;i++){
+                int result=a-b;
+                sum+=result;
+        }
+
+
+
+
+
+
+        }
+        cout<<sum<<endl;
+
+    }
+
+
+
+
+
+
+
